d60be30c26fc8c8b2e5baa60278cd399e767593c
michaelzheng02 <michaelz113@hotmail.com>
2025-05-15 14:25:14 -0400

    chore: create rust project

---
+++ Tokenizer/target/debug/deps/Tokenizer-04594fa1142e880a.d
@@ -0,0 +1,5 @@
+C:\Users\Lettuce\Documents\CISC3160\tokenizer\Tokenizer\Tokenizer\target\debug\deps\libTokenizer-04594fa1142e880a.rmeta: src\main.rs
+
+C:\Users\Lettuce\Documents\CISC3160\tokenizer\Tokenizer\Tokenizer\target\debug\deps\Tokenizer-04594fa1142e880a.d: src\main.rs
+
+src\main.rs:
---
+++ Tokenizer/target/debug/deps/Tokenizer-b5ed15f583821370.d
@@ -0,0 +1,5 @@
+C:\Users\Lettuce\Documents\CISC3160\tokenizer\Tokenizer\Tokenizer\target\debug\deps\libTokenizer-b5ed15f583821370.rmeta: src\main.rs
+
+C:\Users\Lettuce\Documents\CISC3160\tokenizer\Tokenizer\Tokenizer\target\debug\deps\Tokenizer-b5ed15f583821370.d: src\main.rs
+
+src\main.rs:
